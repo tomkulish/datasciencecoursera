9fb8328c527ea5b505297fa125d46b166a4fb5cf
tkulish <thomas.kulish@gmail.com>
2015-01-28 21:37:28 -0500

    Adding inital week 4 work

---
+++ Programming R/Week 4 Project/best.R
@@ -0,0 +1,13 @@
+best <- function(state, outcome) {
+  ## Read outcome data
+  outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  ## Check that state and outcome are valid
+  states <- outcome$states
+  validOutcome <- c("heart attack", "heart failure", "pneumonia")
+  ## Return hospital name in that state with lowest 30-day death
+  ## rate
+}
+
+# outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+#outcome[, 11] <- as.numeric(outcome[, 11])
+#hist(outcome[, 11])
