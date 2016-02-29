ea8c5c8478282b86980ea4ac4c6a472318952167
tomkulish <thomas.kulish@gmail.com>
2016-02-29 12:22:35 -0500

    Added in plot 3,4,5,6

--- Exploratory Data Analysis/project2/project2.R
+++ Exploratory Data Analysis/project2/project2.R
@@ -58,3 +58,92 @@ barplot(
 
 # Save barplot 
 dev.off()
+
+###############
+# Plot 3
+###############
+library(ggplot2)
+# Subset pmData data by Baltimore
+baltimorepmData <- pmData[pmData$fips=="24510",]
+
+par("mar"=c(5.1, 4.5, 4.1, 2.1))
+png(filename = "./plot3.png", width = 480, height = 480, units = "px")
+g <- ggplot(baltimorepmData, aes(year, Emissions, color = type))
+g + geom_line(stat = "summary", fun.y = "sum") + ylab( " Total 2.5 Emissions") +  ggtitle("Total Emissions in Baltimore City from 1999 to 2008")
+dev.off()
+
+
+##############
+# Plot 4
+##############
+library(ggplot2)
+
+# Subset coal combustion related NEI data
+combustionTogether <- grepl("comb", sccData$SCC.Level.One, ignore.case=TRUE)
+coalTogether <- grepl("coal", sccData$SCC.Level.Four, ignore.case=TRUE) 
+coalCombustion <- (combustionTogether & coalTogether)
+combustionSCC <- sccData[coalCombustion,]$SCC
+combustionpmData <- pmData[pmData$SCC %in% combustionSCC,]
+
+png("plot4.png",width=480,height=480,units="px",bg="transparent")
+
+ggp <- ggplot(combustionpmData,aes(factor(year),Emissions/10^5)) +
+  geom_bar(stat="identity",fill="grey",width=0.75) +
+  theme_bw() +  guides(fill=FALSE) +
+  labs(x="year", y="Total PM Emission (10^6 Tons)") + 
+  labs(title="PM Coal Combustion Source Emissions Across US from 1999-2008")
+
+print(ggp)
+
+dev.off()
+
+###############
+# Plot 5 
+##############
+library(ggplot2)
+
+# Combine the two datasets
+#pmDatawithScc <- merge(pmData, sccData, by="SCC")
+
+subsetpmData <- pmData[pmData$fips=="24510" & pmData$type=="ON-ROAD",  ]
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetpmData, sum)
+
+png("plot5.png", width=840, height=480)
+g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
+g <- g + geom_bar(stat="identity") + xlab("year") + ylab("Total PM 2,5 Emissions") + ggtitle('Total Emissions pmData in Baltimore from 1999 to 2008')
+print(g)
+dev.off()
+
+##################
+# Plot 6
+#################
+# Get only things with the vechnical data
+vehicles <- grepl("vehicle", sccData$SCC.Level.Two, ignore.case=TRUE)
+vehiclesSCC <- sccData[vehicles,]$SCC
+vehiclespmData <- pmData[pmData$SCC %in% vehiclesSCC,]
+
+# create the datasets for each city
+vehiclesBaltimorepmData <- vehiclespmData[vehiclespmData$fips=="24510",]
+vehiclesBaltimorepmData$city <- "Baltimore"
+
+vehiclesLApmData <- vehiclespmData[vehiclespmData$fips=="06037",]
+vehiclesLApmData$city <- "Los Angeles County"
+
+# put the two datasets together to do analysis on.
+bothpmData <- rbind(vehiclesBaltimorepmData,vehiclesLApmData)
+
+png("plot6.png",width=480,height=480,units="px",bg="transparent")
+
+library(ggplot2)
+
+ggp <- ggplot(bothpmData, aes(x=factor(year), y=Emissions, fill=city)) +
+  geom_bar(aes(fill=year),stat="identity") +
+  facet_grid(scales="free", space="free", .~city) +
+  guides(fill=FALSE) + theme_bw() +
+  labs(x="year", y="Total PM Emission (Kilo-Tons)") + 
+  labs(title="PM Motor Vehicle Source Emissions in Baltimore & LA, (years: 1999-2008)")
+
+print(ggp)
+
+dev.off()
